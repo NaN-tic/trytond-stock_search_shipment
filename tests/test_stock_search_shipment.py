839d6175c5c6e8b62734221f4e85e34487822309
resteve <resteve@zikzakmedia.com>
2015-12-01 11:26:17 +0100

    Tests

--- tests/test_stock_search_shipment.py
+++ tests/test_stock_search_shipment.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
 # This file is part of the stock_search_shipment module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.tests.test_tryton import test_view, test_depends
-import trytond.tests.test_tryton
 import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class StockSearchShipmentTestCase(unittest.TestCase):
+class StockSearchShipmentTestCase(ModuleTestCase):
     'Test Stock Search Shipment module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('stock_search_shipment')
-
-    def test0005views(self):
-        'Test views'
-        test_view('stock_search_shipment')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'stock_search_shipment'
 
 
 def suite():
